22d92a374e889f7e7b0592e5d7c6113cbeb7f36c
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-06-21 01:04:22 -0700

    make 18 cards explicit

--- make18ps.py
+++ make18ps.py
@@ -4,7 +4,7 @@ import os
 
 outpath = "./18cardsfile/"
 basepath = "template.eps"
-inpath = "cardlist.txt"
+inpath = "18cardlist.txt"
 base = open(basepath) #the template
 template = base.read()
 base.close() #there is nothing more to see
